595a419e2d2256702bbd4ba28508ab43e45aa921
Hayzar <hayzarmuhaiyar@gmail.com>
2025-05-15 19:55:49 +0700

    1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,29 @@ function myfunction(){
     alert("Kan sudah bilang jangan di klik!");
 }
 
-
+document.addEventListener('DOMContentLoaded', function() {
+    const klikBtn = document.querySelector('.klik-btn');
+    if (klikBtn) {
+        klikBtn.addEventListener('click', function() {
+            Swal.fire({
+                title: 'Cieeee Ketipuu! 😝',
+                text: 'Makanya jangan sembarangan pencet-pencet ya! 😂',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                background: '#fff',
+                iconColor: '#2ecc71',
+                customClass: {
+                    popup: 'animated bounceIn'
+                },
+                showClass: {
+                    popup: 'animate__animated animate__bounceIn'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__bounceOut'
+                }
+            });
+        });
+    }
+});
